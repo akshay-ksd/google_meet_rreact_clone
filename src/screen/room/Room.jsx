fdc4421bf066237db00669bdcc29c1eb43574073
akshay-ksd <akshaykarandakkad92@gmail.com>
2023-07-30 13:07:51 +0530

    ddd

--- src/screen/room/Room.jsx
+++ src/screen/room/Room.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { SocketContext } from "../../socket/SocketContext";
 function Room() {
+  const {name,callAccepted,myVideo,userVideo,callEnded,stream,call} = useContext(SocketContext)
   return (
     <div style={{ width: "100%", height: 200, backgroundColor: "white" }}>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
